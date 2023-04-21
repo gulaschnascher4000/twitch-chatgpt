3fea9f9602b612d796e37e1d6b48f4c3245578fa
gulaschnascher4000 <121955064+gulaschnascher4000@users.noreply.github.com>
2023-04-21 03:47:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -74,11 +74,11 @@ app.get('/gpt/:text', async (req, res) => {
       const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo-0301",
         messages: messages,
-        temperature: 0.8,
-        max_tokens: 128,
+        temperature: 0.5,
+        max_tokens: 1024,
         top_p: 1,
-        frequency_penalty: 2,
-        presence_penalty: 2,
+        frequency_penalty: 1,
+        presence_penalty: 1,
       });
     
       if (response.data.choices) {
