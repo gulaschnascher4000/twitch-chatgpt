0b18993379b7a4ed8ee08dbc6e2cbd361af36a4b
gulaschnascher4000 <121955064+gulaschnascher4000@users.noreply.github.com>
2023-04-21 04:47:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -77,8 +77,8 @@ app.get('/gpt/:text', async (req, res) => {
         temperature: 1.3,
         max_tokens: 1024,
         top_p: 1,
-        frequency_penalty: 1.5,
-        presence_penalty: 1.5,
+        frequency_penalty: 0.5,
+        presence_penalty: 0.5,
       });
     
       if (response.data.choices) {
