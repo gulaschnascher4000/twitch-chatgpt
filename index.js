83138eda0f08df5d348d544b6c588ca3c4669a45
gulaschnascher4000 <121955064+gulaschnascher4000@users.noreply.github.com>
2023-04-21 03:09:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,10 +75,10 @@ app.get('/gpt/:text', async (req, res) => {
         model: "gpt-3.5-turbo-0301",
         messages: messages,
         temperature: 0.8,
-        max_tokens: 2048,
+        max_tokens: 4096,
         top_p: 1,
-        frequency_penalty: 1,
-        presence_penalty: 1,
+        frequency_penalty: 2,
+        presence_penalty: 2,
       });
     
       if (response.data.choices) {
@@ -88,9 +88,9 @@ app.get('/gpt/:text', async (req, res) => {
         messages.push({role: "assistant", content: agent_response})
 
         //Check for Twitch max. chat message length limit and slice if needed
-        if(agent_response.length > 499){
+        if(agent_response.length > 399){
           console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-          agent_response = agent_response.substring(0, 499)
+          agent_response = agent_response.substring(0, 399)
           console.log ("Sliced agent answer: " + agent_response)
         }
 
